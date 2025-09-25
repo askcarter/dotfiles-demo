a0d1717796f42bcb7ae9e283619a3719afcb0cdb
Carter Morgan <cartermorgan1321+github@gmail.com>
2025-09-25 06:00:40 +0000

    Added more neovim setup -- removing all the popups and onscreen notifications

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.g.snacks_animate = false -- remove scrilling animation for large files
+vim.g.lazyvim_check_order = false -- remove error warning
+
+vim.opt.ignorecase = true -- make command search case insensitive
+
+-- vim.opt.relativenumber = false   -- uncomment this to disable relative line number
+-- vim.opt.number = false
+vim.opt.scrolloff = 8 -- scroll from the middle of the screen
+
+vim.opt.listchars = false -- remove trailing white space chars and other space chars
